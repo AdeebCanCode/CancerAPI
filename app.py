aec835d3832a5bce23a4f27276f79876b48d7236
Adeeb Khan <94802359+AdeebCanCode@users.noreply.github.com>
2023-10-31 20:09:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI, File, UploadFile
 from keras.models import load_model
-from keras.preprocessing import image
 from keras.applications.vgg19 import preprocess_input
 import numpy as np
 from PIL import Image
 import io
+from pathlib import Path
+
+# Specify the path to your model file
+model_path = Path("model_vgg19.h5")
 
 # Load the pre-trained model
-model = load_model('model_vgg19.h5')
+model = load_model(model_path)
 
 # Create a FastAPI app
 app = FastAPI()
@@ -41,7 +44,10 @@ async def predict_image(file: UploadFile):
     except Exception as e:
         return {"error": "Internal server error"}
 
-# Run the FastAPI app
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+    # Dynamically configure the port using the PORT environment variable
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
